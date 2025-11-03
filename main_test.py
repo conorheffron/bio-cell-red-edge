1a92f4189e5668a9395be941038ca67d7746df94
Conor Heffron <conor.heffron@gmail.com>
2025-11-03 16:57:46 +0000

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -1,2 +1,13 @@
-def test_dummy():
-  pass
+import main
+import pytest
+import sys
+
+
+@pytest.mark.parametrize("files", ["original image cropped.png", None])
+def test_main(decimals):
+  try:
+    sys.argv = files
+    main = main()
+    repr(main)
+  except Exception as e:
+    pytest.fail(f"An error occurred: {e}")
